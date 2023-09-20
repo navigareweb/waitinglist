19e0fe809fad8b591c2b8157ae8b030f2c00055a
navigareweb <web@navigaresrl.com>
2023-09-20 11:18:51 +0200

    first commit

--- app/page.js
+++ app/page.js
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import copertina from "../public/heromiro.jpg";
+import logo from "../public/logo.png";
 import styles from "./index.module.scss";
 import Link from "next/link";
 
@@ -7,15 +8,25 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.copertina}>
-        <Image src={copertina} width={"100%"} className={"img"} />
+        <Image
+          src={copertina}
+          width={"100%"}
+          className={"img"}
+          alt="copertina"
+        />
         <p>
-          La mostra racchiude all’incirca 100 opere tra dipinti, tempere,
-          acquerelli, disegni, sculture e ceramiche provenienti da musei
-          francesi e collezionisti privati, completata da una serie di opere
-          grafiche, libri e documenti. La curatela è affidata al critico d’arte
-          Achille Bonito Oliva, fra i maggiori e più stimati in Italia, insieme
-          a Maïthé Vallès-Bled (già direttrice di musei francesi) e a Vincenzo
-          Sanfo, esperto d’arte e organizzatore di grandi mostre internazionali.
+          Dal 28 Ottobre al 14 Gennaio la mostra Miró a Torino sarà visitabile
+          al Mastio Della Cittadella di Torino (C.so Galileo Ferraris, 0, 10121)
+          Dal Lunedì al Venerdì: dalle ore 09,30 alle ore 19,30. Sabato e
+          Domenica: dalle ore 09,30 alle ore 20,30. Ultimo ingresso trenta
+          minuti prima della chiusura. La mostra racchiude all’incirca 100 opere
+          tra dipinti, tempere, acquerelli, disegni, sculture e ceramiche
+          provenienti da musei francesi e collezionisti privati, completata da
+          una serie di opere grafiche, libri e documenti. La curatela è affidata
+          al critico d’arte Achille Bonito Oliva, fra i maggiori e più stimati
+          in Italia, insieme a Maïthé Vallès-Bled (già direttrice di musei
+          francesi) e a Vincenzo Sanfo, esperto d’arte e organizzatore di grandi
+          mostre internazionali.
         </p>
         <Link
           href={"https://www.navigaresrl.com/mostra/3573/"}
@@ -24,11 +35,16 @@ export default function Home() {
           Info Mostra
         </Link>
         <h3>
-          Iscriviti alla nostra Waiting List per ricevere uno sconto del 30%
+          Iscriviti entro il 15 Ottobre alla nostra Waiting List per ricevere
+          uno sconto del 30%
         </h3>
         <h4>* lo sconto verrà applicato SOLO all'acquisto in biglietteria</h4>
       </div>
-      <form className={styles.form}>
+      <form
+        action="https://formsubmit.co/9342ac848191fdbd8f6fdf241baff761"
+        method="POST"
+        className={styles.form}
+      >
         <input
           type="text"
           id="name"
@@ -53,7 +69,12 @@ export default function Home() {
           </p>
         </div>
       </form>
-      <div className={styles.footer}></div>
+      <div className={styles.footer}>
+        Una produzione di{" "}
+        <Link href={"https://www.navigaresrl.com/"}>
+          <Image src={logo} width={70} alt="logo"></Image>
+        </Link>
+      </div>
     </main>
   );
 }
