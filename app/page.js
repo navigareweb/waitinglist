8202e55caddbf28ef25e1f1147844abbec2ae736
navigareweb <web@navigaresrl.com>
2023-09-20 11:48:15 +0200

    new

--- app/page.js
+++ app/page.js
@@ -15,15 +15,17 @@ export default function Home() {
           alt="copertina"
         />
         <p>
-          Dal 28 Ottobre al 14 Gennaio la mostra Miró a Torino sarà visitabile
-          al Mastio Della Cittadella di Torino. La mostra racchiude all’incirca
-          100 opere tra dipinti, tempere, acquerelli, disegni, sculture e
-          ceramiche provenienti da musei francesi e collezionisti privati,
-          completata da una serie di opere grafiche, libri e documenti. La
-          curatela è affidata al critico d’arte Achille Bonito Oliva, fra i
-          maggiori e più stimati in Italia, insieme a Maïthé Vallès-Bled (già
-          direttrice di musei francesi) e a Vincenzo Sanfo, esperto d’arte e
-          organizzatore di grandi mostre internazionali.
+          Dal <strong>28 Ottobre al 14 Gennaio</strong> la mostra{" "}
+          <strong> Miró a Torino </strong>
+          sarà visitabile al Mastio Della Cittadella di Torino. La mostra
+          racchiude all’incirca 100 opere tra dipinti, tempere, acquerelli,
+          disegni, sculture e ceramiche provenienti da musei francesi e
+          collezionisti privati, completata da una serie di opere grafiche,
+          libri e documenti. La curatela è affidata al critico d’arte{" "}
+          <strong>Achille Bonito Oliva</strong>, fra i maggiori e più stimati in
+          Italia, insieme a Maïthé Vallès-Bled (già direttrice di musei
+          francesi) e a Vincenzo Sanfo, esperto d’arte e organizzatore di grandi
+          mostre internazionali.
         </p>
         <Link
           href={"https://www.navigaresrl.com/mostra/3573/"}
@@ -31,11 +33,11 @@ export default function Home() {
         >
           Info Mostra
         </Link>
-        <h3>
+        <h4>
           Iscriviti entro il 15 Ottobre alla nostra Waiting List per ricevere
           uno sconto del 30%
-        </h3>
-        <h4>* lo sconto verrà applicato SOLO all'acquisto in biglietteria</h4>
+        </h4>
+        <h5>* lo sconto verrà applicato SOLO all'acquisto in biglietteria</h5>
       </div>
       <form
         action="https://formsubmit.co/9342ac848191fdbd8f6fdf241baff761"
